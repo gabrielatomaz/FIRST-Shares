0c3654dfefbc2340e223b43c2409dce536c4c5c1
gabrielatomaz <gabrielatomaz1772@gmail.com>
2019-05-28 17:54:08 -0300

    Adicionado banco do cloud server

--- app/AdminFSMobile/src/screens/Login.js
+++ app/AdminFSMobile/src/screens/Login.js
@@ -14,7 +14,7 @@ const width = Dimensions.get('screen').width;
 
 export default class Login extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             usuario: '',
@@ -23,24 +23,22 @@ export default class Login extends Component {
         }
     }
 
-    efetuaLogin(){
-        const uri = 
-        "https://localhost:44324/api/usuario/" +
-        "RetornarUsuarioAutenticado/" 
-        + this.state.usuario + "/" + this.state.senha;
-
+    efetuaLogin() {
+        const uri ="https://localhost:44324/api/usuario/retornarusuarioautenticado/"
+            + this.state.usuario + "/" + this.state.senha;
+        console.warn(uri);
         fetch(uri)
-        .then(resp => {
-            if(resp.ok)
-                return resp.text();
+            .then(resp => {
+                if (resp.ok)
+                    return resp.text();
+                    
+                throw new Error("Não foi possível efetuar o login.");
+            })
+            .then(token => {
+                AsyncStorage.setItem('userName', this.state.usuario);
 
-            throw new Error("Não foi possível efetuar o login.");
-        })
-        .then(token => {
-            AsyncStorage.setItem('userName', this.state.usuario);
-
-            return AsyncStorage.getItem('userName');
-        }).catch(e => this.setState({mensagem: e.message}));
+                return AsyncStorage.getItem('userName');
+            }).catch(e => this.setState({ mensagem: e.message }));
     }
 
     render() {
@@ -80,14 +78,14 @@ const styles = StyleSheet.create({
         borderBottomColor: '#ddd'
     },
     logo: {
-        height:140,
+        height: 140,
         width: 110,
         marginBottom: 20
     },
     btn: {
         marginTop: 20
     },
-    mensagem:{
+    mensagem: {
         marginTop: 20,
         color: "#e74c3c"
     }
