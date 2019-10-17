d8799d1d0f08e7cafd13a611690ec2e2b3febf39
gabrielatomaz <gabrielatomaz1772@gmail.com>
2019-10-17 08:36:56 -0300

    correção imagem

--- FIRSTShares/Controllers/AccountController.cs
+++ FIRSTShares/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
@@ -66,6 +69,7 @@ namespace FIRSTShares.Controllers
             }
 
             var fotoBytes = AdicionarUsuarioFoto(foto);
+            fotoBytes = ResizeImage(byteArrayToImage(fotoBytes), 400, 400);
 
             var usuarioDb = new Usuario
             {
@@ -100,6 +104,14 @@ namespace FIRSTShares.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+
+            return returnImage;
+        }
+
         public byte[] AdicionarUsuarioFoto(IFormFile foto)
         {
             if(foto == null)
@@ -110,6 +122,34 @@ namespace FIRSTShares.Controllers
                 foto.CopyTo(ms);
                 return ms.ToArray();
             }
+        }
+
+        public byte[] ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            var ms = new MemoryStream();
+            destImage.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+
+            return ms.ToArray();
         }
 
         [HttpPost]
--- FIRSTShares/Controllers/ProfileController.cs
+++ FIRSTShares/Controllers/ProfileController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
@@ -103,12 +106,51 @@ namespace FIRSTShares.Controllers
             using (var ms = new MemoryStream())
             {
                 foto.CopyTo(ms);
-                usaurio.Foto.FotoBase64 = ms.ToArray();
+                var fotoArray = ms.ToArray();
+                fotoArray = ResizeImage(byteArrayToImage(fotoArray), 400, 400);
+                usaurio.Foto.FotoBase64 = fotoArray;
             }
 
             BD.Update(usaurio);
 
             return BD.SaveChanges() > 0;
+        }
+
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+
+            return returnImage;
+        }
+
+        public byte[] ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            MemoryStream ms = new MemoryStream();
+            destImage.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+
+            return ms.ToArray();
         }
 
 
