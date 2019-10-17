eb9f7dd6cfe62aebd9f7330b89541c38e3c84470
Gabriela Tomaz Ribeiro <gabriela@triider.com>
2019-10-17 15:44:20 -0300

    try

--- FIRSTShares/Controllers/AccountController.cs
+++ FIRSTShares/Controllers/AccountController.cs
@@ -127,28 +127,29 @@ namespace FIRSTShares.Controllers
         public byte[] ResizeImage(Image image, int width, int height)
         {
             var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
+            using(var destImage = new Bitmap(width, height){
 
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+                destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
+                using (var graphics = Graphics.FromImage(destImage))
                 {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    using (var wrapMode = new ImageAttributes())
+                    {
+                        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    }
                 }
+
+                var ms = new MemoryStream();
+                destImage.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
             }
-
-            var ms = new MemoryStream();
-            destImage.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
-
+            
             return ms.ToArray();
         }
 
